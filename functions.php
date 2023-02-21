56b722e89ea69683bb3a147b41e74938baeb577a
João Santos <32807182+jvpdls@users.noreply.github.com>
2023-02-21 19:43:43 -0300

    added headers

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php 
 
 function security_headers() {
+	header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
 	header('X-Frame-Options: SAMEORIGIN');
+	header('X-Content-Type-Options:	nosniff');
+	header('Content-Security-Policy: upgrade-insecure-requests');
 	header('Referrer-Policy: strict-origin-when-cross-origin');
-	header("Permissions-Policy: geolocation=(self), camera=(self), microphone=(self), payment=(self)");
+	header('Permissions-Policy: geolocation=(self), camera=(self), microphone=(self), payment=(self)');
 }
 add_action( 'send_headers', 'security_headers' );
